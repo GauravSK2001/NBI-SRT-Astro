af7c8b6be2d1c68f4f5210b47b7a70ec66e4bc62
GauravSK2001 <gaurav.senthil@nbi.ku.dk>
2025-01-15 16:21:54 +0100

    Initialization

---
+++ calibration/Calibration.py
@@ -0,0 +1,94 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy import units as u
+from astropy import constants as const
+from astropy.cosmology import Plack18 as planck18
+
+
+
+
+
+class Atomospheric_effects:
+    
+    
+    def __init__(self,air_temperature):
+        self.optical_depth=0.005 #optical depth of the atmosphere standard value
+        self.air_temperature= air_temperature #temperature of the air in kelvin
+        
+    def Slant_path_optical_depth(self,elavation_angle):
+        theta= np.pi/2 -np.deg2rad(elavation_angle)
+        tau= self.optical_depth/np.cos(theta)
+        return tau
+    
+    def atmospheric_emission(self,elavation_angle):
+        tau= self.Slant_path_optical_depth(elavation_angle)
+        T= self.air_temperature*(1-np.exp(-tau))
+        return T
+    
+    def atmospheric_absorption(self,elavation_angle,Observed_temperature):
+        tau= self.Slant_path_optical_depth(elavation_angle)
+        T= Observed_temperature/(np.exp(-tau))
+        return T
+
+# Calibration class
+class Calibration:
+    '''
+    Y method of calibration
+    
+    '''
+    
+    def __init__(self,calibration_intigration_time,air_temperature):
+        self.calibration_intigration_time= calibration_intigration_time
+        self.air_temperature= air_temperature #temperature of the air in kelvin
+        self.atmospheric= Atomospheric_effects(air_temperature)
+        self.CMB_temperature= planck18.Tcmb0.to(u.K)
+        self.Hot_temperature= 0
+        self.Cold_temperature= 0
+        self.slope= 0
+        self.intercept= 0
+        self.Tsys= 0
+    def cold_temperature(self,elavation_angle):
+        
+        T_cold= self.atmospheric.atmospheric_emission(elavation_angle) + self.CMB_temperature
+        self.Cold_temperature= T_cold
+        return T_cold
+    
+    def Hot_temperature(self,Calibration_surface_temperature):
+        
+        T_hot= Calibration_surface_temperature
+        self.Hot_temperature= T_hot
+        
+        return T_hot
+    
+    def Y_factor(self,Continuum_Hot,Continuum_Cold):
+        return Continuum_Hot/Continuum_Cold
+    
+    def Tsys(self,Continuum_Hot,Continuum_Cold):
+        
+        Y= self.Y_factor(Continuum_Hot,Continuum_Cold)
+        Tsys= (self.Hot_temperature-Y*self.Cold_temperature)/(Y-1)
+        return Tsys
+    
+    def Tcal(self,Power,Continuum_Hot,Continuum_Cold):
+        
+        m= (self.Hot_temperature-self.Cold_temperature)/(Continuum_Hot-Continuum_Cold)
+        b= (-1*(Continuum_Hot/(Continuum_Hot-Continuum_Cold))*(self.Hot_temperature-self.Cold_temperature))+self.Hot_temperature
+        
+        Tcal= m*Power + b
+        return Tcal
+    
+    def Ploter(self):
+        '''
+        TO DO: 
+        1. add the Tcold and Thot to the plot
+        2. add the Tsys to the plot
+        
+        '''
+        x= np.linspace(0,self.Hot_temperature+10,100)
+        y= self.slope*x + self.intercept
+        fig, ax = plt.subplots()
+        ax.plot(x,y)
+        
+        ax.set(xlabel=r'Power ($P$)', ylabel=r'Temperature ($T$) [K]',title='Calibration')
+        ax.grid()
+        plt.show()
---
+++ source_tracking/Source_tracking_test.py
@@ -0,0 +1,94 @@
+import numpy as np
+import serial
+import time
+
+
+class Rot2Prog:
+    def __init__(self):
+        self.port = "/dev/ttyUSB0"
+        self.baudrate = 115200
+        self.serial = serial.Serial(  # Renamed to self.serial
+            port=self.port,
+            baudrate=self.baudrate,
+            bytesize=serial.EIGHTBITS,
+            parity="N",
+            stopbits=serial.STOPBITS_ONE,
+            timeout=None,
+        )
+        self.pulses_per_degree = 10
+        self.az_min = 0
+        self.az_max = 360
+        self.el_min = 0.0
+        self.el_max = 90
+        
+    def send_pkt(self, cmd, az=None, el=None):
+        if az is not None and el is not None:
+            azimuth = int(self.pulses_per_degree * (az + self.az_max))
+            elevation = int(self.pulses_per_degree * (el + self.el_max))
+        else:
+            azimuth = 0
+            elevation = 0
+
+        azimuth_ticks = self.pulses_per_degree
+        elevation_ticks = self.pulses_per_degree
+
+        cmd_string = "W%04d%c%04d%c%c " % (
+            azimuth,
+            azimuth_ticks,
+            elevation,
+            elevation_ticks,
+            cmd,
+        )
+        cmd_bytes = cmd_string.encode("ascii")
+        self.serial.write(cmd_bytes)
+        time.sleep(1)
+
+    def receive_rot2_pkt(self):
+        received_vals = self.serial.read(12)
+        az = (
+            (received_vals[1] * 100)
+            + (received_vals[2] * 10)
+            + received_vals[3]
+            + (received_vals[4] / 10.0)
+            - self.az_max
+            + self.az_min
+        )
+        el = (
+            (received_vals[6] * 100)
+            + (received_vals[7] * 10)
+            + received_vals[8]
+            + (received_vals[9] / 10.0)
+            - self.el_max
+            + self.el_min
+        )
+        return az, el
+    
+    def point(self, az, el):
+        cmd = 0x2F 
+        az_relative = az - self.az_min
+        el_relative = el - self.el_min
+        self.send_pkt(cmd, az=az_relative, el=el_relative)
+        
+    def stop(self):
+        cmd= 0x0f
+        self.send_pkt(cmd)
+        
+    def status(self):
+        cmd = 0x1F 
+        self.send_pkt(cmd)
+        az_relative, el_relative = self.receive_rot2_pkt()
+        time.sleep(2)
+        return az_relative + self.az_min, el_relative + self.el_min
+    
+    def Restart(self):
+        cmd=[0x57,0xef,0xbe,0xad,0xde,0x00,0x00,0x00,0x00,0x00,0x00,0xee,0x20]
+        packet=bytes(cmd)
+        self.serial.write(packet)
+        self.serial.flush()
+        print('Restarting in 5 sec')
+        for i in range(5, 0, -1):
+            print(.)
+            time.sleep(1)  # Wait for 1 second
+        print("...Restarting...")
+        time.sleep(2)
+        print("...Restarted...")
