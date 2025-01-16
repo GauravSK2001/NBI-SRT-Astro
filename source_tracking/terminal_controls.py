2ecc91abc1a1d5da1d0b5c6c9e39492b97b159b5
GauravSK2001 <gaurav.senthil@nbi.ku.dk>
2025-01-16 19:47:11 +0100

    updates of tracking

--- source_tracking/terminal_controls.py
+++ source_tracking/terminal_controls.py
@@ -19,25 +19,58 @@ if __name__=='__main__':
 
         In=Input.split(' ')
 
-        if In[0]=='P':
+        if In[0]=='P': #Pointing
+        
             if len(In)==3:
                 L=float(In[1])
                 B=float(In[2])
                 az,el=conv.Convert(L,B)
+                az,el=int(az),int(el)
                 if conv.Check_if_allowed_el(el):
                     control.point(az,el)
-                    print(f'Pointing Toward ({int(L)},{int(B)})')
+                    print(f'Pointing Toward ({L:.2f},{B:.2f})')
                 else:
                     print(f'Not Valid Pointing for the Time and coordinate')
 
             else:
                 print("Not Valid Entry... Try again")
 
-        if In[0]=="Off":
+        if In[0]=="Off": #Shutdown Telescope
 
             Switch=False
 
-        else:
-            print('Try Again, Invalid')
+        if In[0]=='S': #slewing
+            if len(In)==4: #with Az El Coordinates
+                if In[-1]=='AzEl':
+                    az=float(In[1])
+                    el=float(In[2])
+                    if conv.Check_if_allowed_el(el):
+                        control.point(az,el)
+                        print(f'Slewing Toward ({int(az)},{int(el)})')
+                    else:
+                        print(f'Not Valid Slew Pointing for the Time and coordinate')
+                else:
+                    print("Not Valid Entry... Try again")
+                    
+            elif len(In)==3: #with L B coordinates
+                    L=float(In[1])
+                    B=float(In[2])
+                    az,el=conv.Convert(L,B)
+                    az,el=int(az),int(el)
+                    if conv.Check_if_allowed_el(el):
+                        control.point(az,el)
+                        print(f'Slewing Toward ({L:.2f},{B:.2f})')
+                    else:
+                        print(f'Not Valid Slew Pointing for the Time and coordinate')
+            else:
+                print("Not Valid Entry... Try again")
+                    
 
-        
+
+        if In[0]=='R': # Restart
+            control.Restart()
+            
+            
+        else:
+            print('Invalid Comand')
+
