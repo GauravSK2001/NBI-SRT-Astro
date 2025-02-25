880d87e4d872bb947d8e5a5ebbc3600a049d896c
GauravSK2001 <gaurav.senthil.2001@gmail.com>
2025-02-25 20:33:19 +0100

    Tracking class has some changes and hope it works :)

--- source_tracking/Controls.py
+++ source_tracking/Controls.py
@@ -9,7 +9,7 @@ class Rot2Prog:
     """
     def __init__(self):
         # Update the port to match your device.
-        self.port = "COM4"
+        self.port = "/dev/ttyUSB0"
         self.baudrate = 115200
 
         # Initialize the serial connection.
