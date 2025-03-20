541253b39bd6daa2af420b61885a8d973d9dddef
Gaurav Senthil Kumar <gaurav.senthil.2001@gmail.com>
2025-03-20 16:06:17 +0100

    new processing

--- signal_processing/PPFB.py
+++ signal_processing/PPFB.py
@@ -10,7 +10,6 @@
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import fft
 from gnuradio.fft import window
@@ -70,7 +69,7 @@ class PPFB(gr.top_block, Qt.QWidget):
         self.sinc = sinc = np.sinc(sinc_sample_locations/np.pi)
         self.samp_rate = samp_rate = 6e6
         self.one_sec_display_integration = one_sec_display_integration = 1
-        self.int_time = int_time = 10
+        self.int_time = int_time = 300
         self.Window = Window = sinc
         self.PFB_bandwidth_vector_length = PFB_bandwidth_vector_length = 7e3
         self.HI21 = HI21 = 1420.405751768e6
@@ -159,25 +158,23 @@ class PPFB(gr.top_block, Qt.QWidget):
 
         self._qtgui_vector_sink_f_0_0_win = sip.wrapinstance(self.qtgui_vector_sink_f_0_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_vector_sink_f_0_0_win)
-        self.osmosdr_source_1_1 = osmosdr.source(
-            args="numchan=" + str(1) + " " + 'airspy=0'
+        self.osmosdr_source_1 = osmosdr.source(
+            args="numchan=" + str(1) + " " + 'airspy=0,bias=1,pack=0'
         )
-        self.osmosdr_source_1_1.set_clock_source('external', 0)
-        self.osmosdr_source_1_1.set_time_source('external', 0)
-        self.osmosdr_source_1_1.set_time_now(osmosdr.time_spec_t(time.time()), osmosdr.ALL_MBOARDS)
-        self.osmosdr_source_1_1.set_sample_rate(samp_rate)
-        self.osmosdr_source_1_1.set_center_freq(HI21, 0)
-        self.osmosdr_source_1_1.set_freq_corr(0, 0)
-        self.osmosdr_source_1_1.set_dc_offset_mode(0, 0)
-        self.osmosdr_source_1_1.set_iq_balance_mode(0, 0)
-        self.osmosdr_source_1_1.set_gain_mode(False, 0)
-        self.osmosdr_source_1_1.set_gain(20, 0)
-        self.osmosdr_source_1_1.set_if_gain(0, 0)
-        self.osmosdr_source_1_1.set_bb_gain(0, 0)
-        self.osmosdr_source_1_1.set_antenna('', 0)
-        self.osmosdr_source_1_1.set_bandwidth(0, 0)
+        self.osmosdr_source_1.set_clock_source('external', 0)
+        self.osmosdr_source_1.set_time_source('external', 0)
+        self.osmosdr_source_1.set_sample_rate(samp_rate)
+        self.osmosdr_source_1.set_center_freq(HI21, 0)
+        self.osmosdr_source_1.set_freq_corr(0, 0)
+        self.osmosdr_source_1.set_dc_offset_mode(0, 0)
+        self.osmosdr_source_1.set_iq_balance_mode(0, 0)
+        self.osmosdr_source_1.set_gain_mode(False, 0)
+        self.osmosdr_source_1.set_gain(20, 0)
+        self.osmosdr_source_1.set_if_gain(0, 0)
+        self.osmosdr_source_1.set_bb_gain(0, 0)
+        self.osmosdr_source_1.set_antenna('', 0)
+        self.osmosdr_source_1.set_bandwidth(0, 0)
         self.fft_vxx_0 = fft.fft_vcc(Vector_length, True, window.hanning(Vector_length), True, 6)
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
         self.blocks_stream_to_vector_0_0_0_0_0_2_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, Vector_length)
         self.blocks_stream_to_vector_0_0_0_0_0_2 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, Vector_length)
         self.blocks_stream_to_vector_0_0_0_0_0_1_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, Vector_length)
@@ -203,7 +200,7 @@ class PPFB(gr.top_block, Qt.QWidget):
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc(Window[:Vector_length])
         self.blocks_integrate_xx_0_0 = blocks.integrate_ff((int(one_sec_display_integration*samp_rate/Vector_length)), Vector_length)
         self.blocks_integrate_xx_0 = blocks.integrate_ff((int(int_time*samp_rate/Vector_length)), Vector_length)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*Vector_length, '/Users/gauravsenthilkumar/repositories/NBI-SRT-Astro/calibration/Frequency_Cal', False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*Vector_length, '/Users/gauravsenthilkumar/repositories/NBI-SRT-Astro/calibration/radio_tests/20_03/plane', False)
         self.blocks_file_sink_0.set_unbuffered(False)
         self.blocks_delay_0_0_0_0_2_0_0 = blocks.delay(gr.sizeof_gr_complex*1, (0*Vector_length))
         self.blocks_delay_0_0_0_0_2_0 = blocks.delay(gr.sizeof_gr_complex*1, (7*Vector_length))
@@ -216,26 +213,13 @@ class PPFB(gr.top_block, Qt.QWidget):
         self.blocks_delay_0_0 = blocks.delay(gr.sizeof_gr_complex*1, (2*Vector_length))
         self.blocks_delay_0 = blocks.delay(gr.sizeof_gr_complex*1, Vector_length)
         self.blocks_complex_to_mag_squared_1 = blocks.complex_to_mag_squared(Vector_length)
-        self.blocks_add_xx_1 = blocks.add_vcc(1)
         self.blocks_add_xx_0 = blocks.add_vcc(Vector_length)
-        self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_SIN_WAVE, HI21, (1e-2), np.pi/2, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.fft_vxx_0, 0))
-        self.connect((self.blocks_add_xx_1, 0), (self.blocks_delay_0, 0))
-        self.connect((self.blocks_add_xx_1, 0), (self.blocks_delay_0_0, 0))
-        self.connect((self.blocks_add_xx_1, 0), (self.blocks_delay_0_0_0, 0))
-        self.connect((self.blocks_add_xx_1, 0), (self.blocks_delay_0_0_0_0, 0))
-        self.connect((self.blocks_add_xx_1, 0), (self.blocks_delay_0_0_0_0_0, 0))
-        self.connect((self.blocks_add_xx_1, 0), (self.blocks_delay_0_0_0_0_1, 0))
-        self.connect((self.blocks_add_xx_1, 0), (self.blocks_delay_0_0_0_0_1_0, 0))
-        self.connect((self.blocks_add_xx_1, 0), (self.blocks_delay_0_0_0_0_2, 0))
-        self.connect((self.blocks_add_xx_1, 0), (self.blocks_delay_0_0_0_0_2_0, 0))
-        self.connect((self.blocks_add_xx_1, 0), (self.blocks_delay_0_0_0_0_2_0_0, 0))
         self.connect((self.blocks_complex_to_mag_squared_1, 0), (self.blocks_integrate_xx_0, 0))
         self.connect((self.blocks_complex_to_mag_squared_1, 0), (self.blocks_integrate_xx_0_0, 0))
         self.connect((self.blocks_delay_0, 0), (self.blocks_stream_to_vector_0_0, 0))
@@ -274,9 +258,17 @@ class PPFB(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_stream_to_vector_0_0_0_0_0_1_0, 0), (self.blocks_multiply_const_vxx_0_0_0_0_0_1_0, 0))
         self.connect((self.blocks_stream_to_vector_0_0_0_0_0_2, 0), (self.blocks_multiply_const_vxx_0_0_0_0_0_2, 0))
         self.connect((self.blocks_stream_to_vector_0_0_0_0_0_2_0, 0), (self.blocks_multiply_const_vxx_0_0_0_0_0_2_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.blocks_add_xx_1, 0))
         self.connect((self.fft_vxx_0, 0), (self.blocks_complex_to_mag_squared_1, 0))
-        self.connect((self.osmosdr_source_1_1, 0), (self.blocks_add_xx_1, 1))
+        self.connect((self.osmosdr_source_1, 0), (self.blocks_delay_0, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.blocks_delay_0_0, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.blocks_delay_0_0_0, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.blocks_delay_0_0_0_0, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.blocks_delay_0_0_0_0_0, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.blocks_delay_0_0_0_0_1, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.blocks_delay_0_0_0_0_1_0, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.blocks_delay_0_0_0_0_2, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.blocks_delay_0_0_0_0_2_0, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.blocks_delay_0_0_0_0_2_0_0, 0))
 
 
     def closeEvent(self, event):
@@ -346,9 +338,7 @@ class PPFB(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate
         self.set_Bandwidth(self.samp_rate)
         self.set_Frequency_Step_Size(self.samp_rate/self.Vector_length)
-        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
-        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
-        self.osmosdr_source_1_1.set_sample_rate(self.samp_rate)
+        self.osmosdr_source_1.set_sample_rate(self.samp_rate)
         self.qtgui_vector_sink_f_0_0.set_x_axis(((-self.samp_rate/2)/1e6), ((self.Frequency_Step_Size)/1e6))
         self.qtgui_vector_sink_f_0_0_0.set_x_axis(((self.HI21-self.samp_rate/2)/1e6), ((self.Frequency_Step_Size)/1e6))
 
@@ -393,8 +383,7 @@ class PPFB(gr.top_block, Qt.QWidget):
 
     def set_HI21(self, HI21):
         self.HI21 = HI21
-        self.analog_sig_source_x_0_0.set_frequency(self.HI21)
-        self.osmosdr_source_1_1.set_center_freq(self.HI21, 0)
+        self.osmosdr_source_1.set_center_freq(self.HI21, 0)
         self.qtgui_vector_sink_f_0_0_0.set_x_axis(((self.HI21-self.samp_rate/2)/1e6), ((self.Frequency_Step_Size)/1e6))
 
     def get_Frequency_Step_Size(self):
